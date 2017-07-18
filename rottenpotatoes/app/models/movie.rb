b30d5b7cef7e746da59eee04147b89354bc9b94f
Luis Alba <lual96@berkeley.edu>
2017-07-18 03:23:44 +0000

    Cucumber tests done

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  
+  def self.director_movies(movie_name)
+    movie = Movie.find_by(title: movie_name)
+    return nil if movie.nil?
+    Movie.where(director: movie.director).pluck(:title)
+  end
+    
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,6 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  
+  get 'samedirector/:title'=> 'movies#director', as: :search_samedirector
 end
